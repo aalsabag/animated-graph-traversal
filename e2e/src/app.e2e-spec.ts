848a77aac37574ac2992fac4281ecc0e32c42051
aalsabag <aalsabag@AAlsabag.localdomain>
2020-10-18 13:06:50 -0500

    initial commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('animatedGraphTraversal app is running!');
+    expect(page.getTitleText()).toEqual('animated-graph-traversal app is running!');
   });
 
   afterEach(async () => {
